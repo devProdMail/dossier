a1115bda8666820b8db6a35bbf24d817476f2830
devProdMail <dev.prod.mail@gmail.com>
2018-05-15 10:42:09 +0200

    Première version de mon projet

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Mon Titre</title>
+		
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	
+	<body>
+		<h1>Hello world !</h1>
+		
+		<script src="js/script.js"></script>
+	</body>
+</html>
